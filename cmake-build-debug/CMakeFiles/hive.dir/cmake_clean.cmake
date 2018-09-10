2b5fd6bc0328a62ea937157b4fdb51d588119d91
Denny Scott <dennyscott301@gmail.com>
2018-09-09 17:07:22 -0700

    initial

---
+++ cmake-build-debug/CMakeFiles/hive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hive.dir/src/main.cpp.o"
+  "hive.pdb"
+  "hive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
